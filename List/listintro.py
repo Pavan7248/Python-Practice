46b16bc40002f1cd013b117af4fc086746a08243
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-10-19 06:01:25 +0000

    Initial commit

---
+++ List/listintro.py
@@ -0,0 +1,20 @@
+list = [1, 2, 3, 4, "Pavan", "Navdeep" ]
+print(list)
+print(list[0])
+print(type(list))
+print(len(list))
+print(list[-1])
+print(list[-2])
+print(list[-3])
+if(4 in list):
+  print("Yes")
+else:
+  print("No")
+
+print(list[1:5:2])
+
+#List comprehension
+list = [x for x in range(10)]
+print(list)
+list = [x*x for x in range(10) if x%2==0]
+print(list)
