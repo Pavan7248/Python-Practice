3a68869924a47b346b32d3b04969ad8c0d73f2fb
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-11-01 15:18:50 +0000

    added a program to print name

---
+++ Programs/name.py
@@ -0,0 +1,5 @@
+print("Program to accepts the user's first and last name and prints them in reverse order with a space between them")
+
+fname = input(str("Enter your first name:- "))
+lname = input(str("Enter your last name:- "))
+print(lname + " " + fname)
