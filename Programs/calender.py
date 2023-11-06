3abf3320b4bf622a21f1ad9ced754419fda81f40
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-11-06 09:27:21 +0000

    added a program to find volume
    it finds the volume
    of the cylinder

--- Programs/calender.py
+++ Programs/calender.py
@@ -3,4 +3,4 @@ import calendar
 y = int(input("Enter the year: "))
 m = int(input("Enter the month: "))
 print(calendar.month(y, m))
-print(calendar.month(y, m, w=2))
+
---
+++ Programs/volume.py
@@ -0,0 +1,5 @@
+pi = 3.144
+r = float(input("Enter the radius: "))
+h = float(input("Enter the height: "))
+v = pi * r * r * h
+print("The volume of the cylinder is: ", v)
