52027ee1659d061804ceff4dd4e10b4fd2ff80f2
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-11-07 09:13:16 +0000

    added finally keyword

--- Programs/multiplication.py
+++ Programs/multiplication.py
@@ -1,9 +1,13 @@
-#Program to write the multiplication tab;le of a number entered b y user
-a = int(input("Enter the number: "))
+#Program to write the multiplication table of a number entered b y user
+a = (input("Enter the number: "))
 print(f"Multiplication table of {a} is given below:")
 #Using Exception Handling 
 try:
   for i in range(1,11):
-    print(f"{a} x {i} = {a*i}")
-except:
+    print(f"{int(a)} * {i} = {int(a)*i}")
+except Exception:
   print("Please enter a valid number")
+
+#Using finally keyword
+finally:
+  print("Thank you for using this program")
