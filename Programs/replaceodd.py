293e1271c9f9d4991ecd0dae7ebe0a8eddbbb91d
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-12-11 08:35:34 +0000

    modied content

--- Programs/replaceodd.py
+++ Programs/replaceodd.py
@@ -1,9 +1,11 @@
-def replace_odd_with_minus_one(arr):
-  return [-1 if num % 2 != 0 else num for num in arr]
+# replace all odd numbers with -1
+
+def replace_odd_with_minus_one(list):
+  return [-1 if num % 2 != 0 else num for num in list]
 
 # Example array
-given_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+given_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Replacing odd numbers with -1
-result_array = replace_odd_with_minus_one(given_array)
-print(result_array)
+result_list = replace_odd_with_minus_one(given_list)
+print(result_list)
